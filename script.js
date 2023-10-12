bee52960f3c22592e8770648de52783f1c51fb40
Peyton Weber <weberpeyton11@gmail.com>
2023-10-12 17:12:39 -0500

    Added user choice prompts and set requirements for min and max characters in password

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
 // Assignment Code
+
 function generatePassword() {
-    var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+     "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+    var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+     "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
     var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+
+    var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/",
+     "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+
     var selectedArray = []; 
 
+    var passwordLength = (setPwLength());
+    var lowerCaseChoice = "";
+    var upperCaseChoice = "";
+    var numericChoice = "";
+    var specialCharacterChoice = "";
+
+    lowerCaseChoice = window.prompt("Would you like to include lower case characters? (yes/no):");
+    if (lowerCaseChoice == "yes") {
+        selectedArray = selectedArray.concat(lowerCaseSet);
+    }
+
+    upperCaseChoice = window.prompt("Would you like to include upper case characters? (yes/no):");
+    if (upperCaseChoice == "yes") {
+        selectedArray = selectedArray.concat(upperCaseSet);
+    }
+
+    numericChoice = window.prompt("Would you like to include numeric characters? (yes/no):");
+    if (numericChoice == "yes") {
+        selectedArray = selectedArray.concat(numSet);
+    }
+
+    specialCharacterChoice = window.prompt("Would you like to include special characters? (yes/no):");
+    if (specialCharacterChoice == "yes") {
+        selectedArray = selectedArray.concat(specialSet);
+    }
 }
+
+function setPwLength() {
+    var PwLength = 0;
+    while ((PwLength < 8) || (PwLength > 128)) {
+        PwLength = window.prompt("How long would you like your password to be? (Must be between 8 and 128):");
+    }
+    return PwLength;
+}
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
