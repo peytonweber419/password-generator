c5a8d773352f617196cbdf1e75c26f66242f9771
Peyton Weber <weberpeyton11@gmail.com>
2023-10-12 15:33:39 -0500

    added arrays

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 // Assignment Code
+function generatePassword() {
+    var lowerCaseSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var upperCaseSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var specialSet = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
+    var selectedArray = []; 
+
+}
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
